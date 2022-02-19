d30734113cb30c3e1e6e5b1523ac07596e054e18
Yoshua Wuyts <yoshuawuyts@gmail.com>
2022-02-19 03:05:49 +0100

    done for the day

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,35 @@
-//! async time combinators
+//! Async time combinators
 //!
-//! # Examples
+//! # Deadlines and Boundaries
 //!
-//! ```text
-//! // tbi
-//! ```
+//! While time is the most common type to use as an event source, other sources
+//! of events also exist. In order to enable this, all functions take a generic
+//! of either `Deadline` or `Boundary`.
+//!
+//! - `Deadline` represents a specific one-off event. Types which implement
+//!   `Deadline` are `std::time::Instant` and `Future`.
+//! - `Boundary` represents a range. Types which implement `Boundary` are `Duration`,
+//!   and `Stream`.
+//!
+//! # Futures
+//! - [`task::sleep`]
+//! - [`Future::delay`](`future::FutureExt::delay`)
+//! - [`Future::delay_until`](`future::FutureExt::delay_until`)
+//! - [`Future::timeout`](`future::FutureExt::timeout`)
+//! - [`Future::timeout_at`](`future::FutureExt::timeout_at`)
+//!
+//! # Streams
+//!
+//! - `stream::interval`
+//! - `Stream::audit`
+//! - `Stream::buffer`
+//! - [`Stream::debounce`](`stream::StreamExt::debounce`)
+//! - [`Stream::delay`](`stream::StreamExt::delay`)
+//! - [`Stream::delay_until`](`stream::StreamExt::delay_until`)
+//! - `Stream::sample`
+//! - [`Stream::throttle`](`stream::StreamExt::throttle`)
+//! - [`Stream::timeout`](`stream::StreamExt::timeout`)
+//! - [`Stream::timeout_at`](`stream::StreamExt::timeout_at`)
 
 #![forbid(unsafe_code)]
 #![deny(missing_debug_implementations)]
