670481b417713a88e5fb7c7b3d3db82b3447bfef
Yoshua Wuyts <yoshuawuyts@gmail.com>
2022-03-31 00:18:31 +0200

    future timeout docs

--- src/future/future_ext.rs
+++ src/future/future_ext.rs
@@ -5,6 +5,30 @@ use super::{Delay, IntoFuture, Timeout};
 /// Extend `Future` with time-based operations.
 pub trait FutureExt: Future {
     /// Await a future or times out after a duration of time.     
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use futures_time::prelude::*;
+    /// use futures_time::time::{Instant, Duration};
+    /// use std::io;
+    ///
+    /// fn main() {
+    ///     async_io::block_on(async {
+    ///         let res = async { "meow" }
+    ///             .delay(Duration::from_millis(100))  // longer delay
+    ///             .timeout(Duration::from_millis(50)) // shorter timeout
+    ///             .await;
+    ///         assert_eq!(res.unwrap_err().kind(), io::ErrorKind::TimedOut); // error
+    ///
+    ///         let res = async { "meow" }
+    ///             .delay(Duration::from_millis(50))    // shorter delay
+    ///             .timeout(Duration::from_millis(100)) // longer timeout
+    ///             .await;
+    ///         assert_eq!(res.unwrap(), "meow"); // success
+    ///     });
+    /// }
+    /// ```
     fn timeout<D>(self, deadline: D) -> Timeout<Self, D::IntoFuture>
     where
         Self: Sized,
@@ -28,9 +52,7 @@ pub trait FutureExt: Future {
     ///     async_io::block_on(async {
     ///         let now = Instant::now();
     ///         let delay = Duration::from_millis(100);
-    ///         let _ = async { "hello world" }
-    ///             .delay(delay)
-    ///             .await;
+    ///         let _ = async { "meow" }.delay(delay).await;
     ///         assert!(now.elapsed() >= *delay);
     ///     });
     /// }
